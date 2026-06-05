8e590aeb916288e24cfbcf5d8eea840bbf468c98
Tim <timothee.bull@gmail.com>
2026-06-05 17:54:05 +0200

    refactor(main): extract app intialization to dedicated func

--- main.go
+++ main.go
@@ -8,27 +8,37 @@ import (
 )
 
 func main() {
+	appState, cmds := initStateAndCommands()
+	command := buildCommandFrom(os.Args)
+
+	err := cmds.run(appState, command)
+	if err != nil {
+		log.Fatalf("- Error: %v", err)
+	}
+}
+
+func initStateAndCommands() (*state, commands) {
 	cfg, err := config.Read()
+	appState := &state{cfg: &cfg}
 	if err != nil {
 		log.Fatalf("- Error reading config: %v", err)
 	}
 
-	appState := &state{cfg: &cfg}
 	cmds := commands{
 		registered: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
 
-	args := os.Args
+	return appState, cmds
+}
+
+func buildCommandFrom(args []string) command {
 	if len(args) < 2 {
 		log.Fatal("Error: please enter a command name")
 	}
 	command := command{name: args[1], args: args[2:]}
 
-	err = cmds.run(appState, command)
-	if err != nil {
-		log.Fatalf("- Error: %v", err)
-	}
+	return command
 }
 
 type state struct {
